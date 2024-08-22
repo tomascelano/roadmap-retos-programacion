2bf69dba7956ee29ecff014e8cecec0e8350023c
tomascelano <122935709+tomascelano@users.noreply.github.com>
2024-08-22 04:50:34 -0300

    tomascelano.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tomascelano.py
@@ -0,0 +1,30 @@
+# https://python.org
+
+# Comentario en una linea
+
+"""
+Esto es un comentario
+en varias
+lineas
+"""
+
+'''
+Esto tambien 
+es un comentario
+en varias
+lineas
+'''
+
+variable_string = "Hola mundo"
+my_constant = "Variable constante"
+
+'''En python existen cuatro tipo de datos primitivos
+ que existen en todos los demas lenguajes de programacion'''
+
+variable_int = 1
+variable_float = 2.88
+variable_booleana = True 
+variable_booleana = False
+variable_string = 'Hola mundo! ' 
+
+print(variable_string + "Python")
